05899b5427394d6cd4f5b29f8037b0cbfaf1ff32
Vladimir Mitev <vladimirmitev6969@gmail.com>
2026-04-08 09:21:23 +0300

    Solve 03.People exercise

---
+++ 01. Intro to Angular and TypeScript/03.People.ts
@@ -0,0 +1,59 @@
+abstract class Employee {
+    public name: string;
+    public age: number;
+    public salary: number;
+    public tasks: Array<string>;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+        this.salary = 0;
+        this.tasks = [];
+    }
+
+    public work(): void {
+        const currentTask = this.tasks.shift();
+        this.tasks.push(currentTask!);
+
+        console.log(this.name + currentTask);
+    }
+
+    public collectSalary(): void {
+        console.log(`${this.name} recieved ${this.getSalary()} this month.`);
+    }
+
+    public getSalary(): number {
+        return this.salary;
+    }
+}
+
+class Junior extends Employee {
+    constructor(name: string, age: number) {
+        super(name, age);
+        this.tasks.push(`${name} is working on a simple task.`);
+    }
+}
+
+class Senior extends Employee {
+    constructor(name: string, age: number) {
+        super(name, age);
+        this.tasks.push(`${name} is working on a comlicated task.`);
+        this.tasks.push(`${name} is taking time off work.`);
+        this.tasks.push(`${name} is supervasing junior workers.`);
+    }
+}
+
+class Manager extends Employee {
+    public divident: number;
+
+    constructor(name: string, age: number) {
+        super(name, age);
+        this.divident = 0;
+        this.tasks.push(`${name} scheduled a meeting.`);
+        this.tasks.push(`${name} is preparing a quarterly meeting.`);
+    }
+
+    public getSalary(): number {
+        return this.salary + this.divident;
+    }
+}
